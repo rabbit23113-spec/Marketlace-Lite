614d63083fa475428dd6a16678a623c10768662c
Raffinnaisse <rabbit23113@gmail.com>
2026-07-19 16:29:14 +0300

    chore: remove cart when deleting user

--- marketplace/apps/users/src/app/cqrs/handlers/deleteUser.handler.ts
+++ marketplace/apps/users/src/app/cqrs/handlers/deleteUser.handler.ts
@@ -3,13 +3,15 @@ import {DeleteUserCommand} from "../commands/deleteUser.command";
 import {InjectRepository} from "@nestjs/typeorm";
 import {UserEntity} from "../../common/entities/user.entity";
 import {Repository} from "typeorm";
-import {NotFoundException} from "@nestjs/common";
+import {Inject, NotFoundException} from "@nestjs/common";
 import {PinoLogger} from "nestjs-pino";
+import {ClientProxy} from "@nestjs/microservices";
 
 @CommandHandler(DeleteUserCommand)
 export class DeleteUserHandler implements ICommandHandler<DeleteUserCommand> {
   constructor(
     @InjectRepository(UserEntity) private repository: Repository<UserEntity>,
+    @Inject("CARTS_CLIENT") private cartsClient: ClientProxy,
     private pino: PinoLogger,
   ) {
   }
@@ -19,6 +21,7 @@ export class DeleteUserHandler implements ICommandHandler<DeleteUserCommand> {
     const user: UserEntity | null = await this.repository.findOneBy({userId});
     if (!user) throw new NotFoundException("User does not exist");
     await this.repository.delete(userId);
+    this.cartsClient.emit("carts.delete", {userId});
     this.pino.info(`USER WITH ID ${userId} DELETED`)
   }
 }
