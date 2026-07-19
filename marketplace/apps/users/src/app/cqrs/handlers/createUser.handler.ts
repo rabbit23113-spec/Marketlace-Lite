0304bf60f77af78ddca338b718b9e62730cad309
Raffinnaisse <rabbit23113@gmail.com>
2026-07-19 16:04:09 +0300

    chore: pino

--- marketplace/apps/users/src/app/cqrs/handlers/createUser.handler.ts
+++ marketplace/apps/users/src/app/cqrs/handlers/createUser.handler.ts
@@ -3,16 +3,23 @@ import {CreateUserCommand} from "../commands/createUser.command";
 import {Repository} from "typeorm";
 import {UserEntity} from "../../common/entities/user.entity";
 import {InjectRepository} from "@nestjs/typeorm";
+import {Inject} from "@nestjs/common";
+import {ClientProxy} from "@nestjs/microservices";
+import {firstValueFrom} from "rxjs";
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
-  constructor(@InjectRepository(UserEntity) private repository: Repository<UserEntity>) {
+  constructor(
+    @InjectRepository(UserEntity) private repository: Repository<UserEntity>,
+    @Inject("CARTS_CLIENT") private cartsClient: ClientProxy,
+  ) {
   }
 
   async execute(command: CreateUserCommand): Promise<UserEntity> {
     const {dto} = command;
     const user = this.repository.create(dto);
     await this.repository.save(user);
+    const cart = await firstValueFrom(this.cartsClient.send("carts.create", {userId: user.userId}));
     return user;
   }
 }
