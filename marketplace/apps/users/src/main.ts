9441139e166f639f338f4535193336f8d57848e5
Raffinnaisse <rabbit23113@gmail.com>
2026-07-19 11:13:04 +0300

    chore: main.ts

--- marketplace/apps/users/src/main.ts
+++ marketplace/apps/users/src/main.ts
@@ -1,20 +1,19 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import {Logger} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app/app.module';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      queue: "USERS_QUEUE",
+      urls: ["amqp://rabbitmq:5672"],
+    }
+  })
+  await app.listen();
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Users service is running`,
   );
 }
 
