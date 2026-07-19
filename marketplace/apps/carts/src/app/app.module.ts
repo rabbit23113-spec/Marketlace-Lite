4c00cd7dffa2794405ed76c2cb743a7838e322e7
Raffinnaisse <rabbit23113@gmail.com>
2026-07-19 14:11:00 +0300

    chore: module and main.ts

--- marketplace/apps/carts/src/app/app.module.ts
+++ marketplace/apps/carts/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {CqrsModule} from "@nestjs/cqrs";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    username: 'root',
+    password: 'root',
+    database: 'carts',
+    synchronize: true,
+    autoLoadEntities: true,
+    entities: [],
+  }),
+    TypeOrmModule.forFeature([]),
+    CqrsModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
