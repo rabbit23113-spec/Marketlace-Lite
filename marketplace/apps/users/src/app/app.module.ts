9af5f056ce72180bf35cb020a4453974759a09b8
Raffinnaisse <rabbit23113@gmail.com>
2026-07-19 12:22:09 +0300

    chore: typeorm

--- marketplace/apps/users/src/app/app.module.ts
+++ marketplace/apps/users/src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    username: 'root',
+    password: 'root',
+    database: 'users',
+    synchronize: true,
+    autoLoadEntities: true,
+    entities: [],
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
