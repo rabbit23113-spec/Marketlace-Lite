7764ea0c318158ade3110e0bc0a4bd63b4d8b02a
Raffinnaisse <rabbit23113@gmail.com>
2026-07-19 14:26:00 +0300

    chore: register transport to users

--- marketplace/apps/carts/src/app/app.module.ts
+++ marketplace/apps/carts/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {CqrsModule} from "@nestjs/cqrs";
 import {CartEntity} from "./common/entities/cart.entity";
+import {ClientsModule, Transport} from "@nestjs/microservices";
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -17,8 +18,19 @@ import {CartEntity} from "./common/entities/cart.entity";
   }),
     TypeOrmModule.forFeature([CartEntity]),
     CqrsModule.forRoot(),
+    ClientsModule.register([
+      {
+        name: "USERS_CLIENT",
+        transport: Transport.RMQ,
+        options: {
+          queue: "USERS_QUEUE",
+          urls: ["amqp://rabbitmq:5672"]
+        }
+      }
+    ])
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
