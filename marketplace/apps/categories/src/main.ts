2718f6d09031e07e7f971b963381ba7c15a61c78
Raffinnaisse <rabbit23113@gmail.com>
2026-07-20 12:45:42 +0300

    chore: init categories

---
+++ marketplace/apps/categories/src/main.ts
@@ -0,0 +1,20 @@
+import {Logger} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app/app.module';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      queue: "CATEGORIES_QUEUE",
+      urls: ["amqp://rabbitmq:5672"],
+    }
+  })
+  await app.listen();
+  Logger.log(
+    `🚀 Categories service is running`,
+  );
+}
+
+bootstrap();
