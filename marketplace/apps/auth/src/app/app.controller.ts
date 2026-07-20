a67279bd32942e95d0d6e81c300d6532a06260d8
Raffinnaisse <rabbit23113@gmail.com>
2026-07-20 10:14:45 +0300

    feat: find one by id

--- marketplace/apps/auth/src/app/app.controller.ts
+++ marketplace/apps/auth/src/app/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller} from '@nestjs/common';
 import { AppService } from './app.service';
+import {MessagePattern, Payload} from "@nestjs/microservices";
+import {SessionEntity} from "./common/entities/session.entity";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  @MessagePattern("auth.findOneById")
+  async findOneById(@Payload() payload: { sessionId: string }): Promise<SessionEntity> {
+    return await this.appService.findOneById(payload.sessionId);
   }
 }
--- marketplace/apps/auth/src/app/app.service.ts
+++ marketplace/apps/auth/src/app/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {CommandBus, QueryBus} from "@nestjs/cqrs";
+import {SessionEntity} from "./common/entities/session.entity";
+import {FindOneByIdQuery} from "./cqrs/queries/findOneById.query";
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  constructor(private readonly queryBus: QueryBus, private readonly commandBus: CommandBus) {
+  }
+
+  async findOneById(sessionId: string): Promise<SessionEntity> {
+    return await this.queryBus.execute(new FindOneByIdQuery(sessionId));
   }
 }
