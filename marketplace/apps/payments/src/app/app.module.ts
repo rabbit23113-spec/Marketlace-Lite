2325671a2f889dd4ede6e8fbba1eadf45bc0c157
Raffinnaisse <rabbit23113@gmail.com>
2026-07-21 13:21:43 +0300

    chore: init payments

---
+++ marketplace/apps/payments/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {CqrsModule} from "@nestjs/cqrs";
+import {LoggerModule} from "nestjs-pino";
+import {ConfigModule} from "@nestjs/config";
+import {YookassaModule} from '@companix/yookassa';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    username: 'root',
+    password: 'root',
+    database: 'payments',
+    synchronize: true,
+    autoLoadEntities: true,
+    entities: [],
+  }),
+    TypeOrmModule.forFeature([]),
+    CqrsModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV === "production" ? "info" : "debug",
+      }
+    }),
+    ConfigModule.forRoot(),
+    YookassaModule.forRoot({
+      shopId: String(process.env.YOOKASSA_SHOP_ID),
+      apiKey: String(process.env.YOOKASSA_API_KEY)
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
