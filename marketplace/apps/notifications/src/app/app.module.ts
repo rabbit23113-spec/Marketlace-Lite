e08afabe129c4fb0ad3d52d437c9cda156142e47
Raffinnaisse <rabbit23113@gmail.com>
2026-07-21 11:53:45 +0300

    chore: init notifications

---
+++ marketplace/apps/notifications/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {LoggerModule} from "nestjs-pino";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {CqrsModule} from "@nestjs/cqrs";
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    username: 'root',
+    password: 'root',
+    database: 'notifications',
+    synchronize: true,
+    autoLoadEntities: true,
+    entities: [],
+  }),
+    TypeOrmModule.forFeature([]),
+    CqrsModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV === "production" ? "info" : "debug",
+      }
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
