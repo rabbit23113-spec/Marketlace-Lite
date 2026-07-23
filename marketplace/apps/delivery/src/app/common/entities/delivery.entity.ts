70700656966882fe4736ddffd3a761fdb4302d4d
Raffinnaisse <rabbit23113@gmail.com>
2026-07-23 11:23:40 +0300

    chore: delivery entity

---
+++ marketplace/apps/delivery/src/app/common/entities/delivery.entity.ts
@@ -0,0 +1,23 @@
+import {Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+import {DeliveryStatus} from "../enums/status.enum";
+
+@Entity()
+export class DeliveryEntity {
+  @PrimaryGeneratedColumn("uuid", {name: "delivery_id"})
+  deliveryId: string;
+
+  @Column({name: "order_id"})
+  orderId: string;
+
+  @Column({name: "status", type: "enum", enum: DeliveryStatus})
+  status: DeliveryStatus;
+
+  @Column({name: "address"})
+  address: string;
+
+  @CreateDateColumn({name: "created_at"})
+  createdAt: Date;
+
+  @UpdateDateColumn({name: "updated_at"})
+  updatedAt: Date;
+}
---
+++ marketplace/apps/delivery/src/app/common/enums/status.enum.ts
@@ -0,0 +1,6 @@
+export enum DeliveryStatus {
+  PENDING = "pending",
+  IN_TRANSIT = "inTransit",
+  CANCELED = "canceled",
+  FAILED = "failed",
+}
