de7bf0fc728a1ce2bdfadb446f5db3103ca9f76c
Raffinnaisse <rabbit23113@gmail.com>
2026-07-23 11:49:34 +0300

    chore: init orders

---
+++ marketplace/apps/orders/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import {LoggerModule} from "nestjs-pino";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {CqrsModule} from "@nestjs/cqrs";
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    username: 'root',
+    password: 'root',
+    database: 'orders',
+    synchronize: true,
+    autoLoadEntities: true,
+    entities: [],
+  }),
+    TypeOrmModule.forFeature([]),
+    CqrsModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV === "production" ? "info" : "debug",
+      }
+    }),
+    ClientsModule.register([
+      {
+        name: "EVENTS_CLIENT",
+        transport: Transport.RMQ,
+        options: {
+          queue: "EVENTS_QUEUE",
+          urls: ["amqp://rabbitmq:5672"]
+        }
+      },
+    ])
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+}
